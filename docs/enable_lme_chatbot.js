b0e0207af4b0d8b05a0e62fe28814eefdaaca658
owais <hello@letmeexplain.ai>
2025-01-30 17:49:02 +0530

    Implementation of Letmeexplain.ai

---
+++ docs/enable_lme_chatbot.js
@@ -0,0 +1,18 @@
+/* This code enables the integration of the  LetMeExplain.ai chatbot to this page. 
+
+Parameters:  orgId, - Unique identifier of the chatbot in the letmeexplain.ai client dashboard.
+For support of LetMeExplain.ai chatbot or access to client dashboard, please contact
+
+Email: hello@letmeexplain.ai 
+Discord ID: @minus1618
+Telegram: @amiri_uk
+
+*/
+
+window.addEventListener("DOMContentLoaded", function() {
+    if (window.loadCustomWidget) {
+        window.loadCustomWidget({
+            orgId: "0c0a4892-3f0c-4a"
+        });
+    }
+});
